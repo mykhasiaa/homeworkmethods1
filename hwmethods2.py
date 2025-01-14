9527ce115c6aee3e37cece3a9c4856c3d405c219
mykhasiaa <mykhailyna.slav@gmail.com>
2025-01-14 19:38:46 +0200

    Code 2
    Code 2 added

---
+++ hwmethods2.py
@@ -0,0 +1,9 @@
+title = "---- Menu ----"
+option1 = "Start Game"
+option2 = "Options"
+option3 = "Exit"
+width = 45
+print(title.center(width, "-"))
+print(option1.center(width))
+print(option2.center(width))
+print(option3.center(width))
